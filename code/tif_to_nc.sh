6ddfa03a7124fb6b31382dbf06c4f7fa0034fb30
rodrigoms95 <rodrigoms95@gmail.com>
2022-08-24 20:03:08 -0500

    tif_to_nc

--- code/tif_to_nc.sh
+++ code/tif_to_nc.sh
@@ -4,17 +4,12 @@
 set -e
 
 # Default options for conda and user.
-FTYPE='Historical'
 RES='10m'
-VAR='elev'
-
 # Read arguments.
-while getopts f:r:v flag
+while getopts r flag
 do
     case '${flag}' in
-        f) FTYPE=${OPTARG};;
         r) RES=${OPTARG};;
-        v) VAR=${OPTARG};;
     esac
 done
 
@@ -24,15 +19,61 @@ source $CONDA_SOURCE'/etc/profile.d/conda.sh'
 # Cambiar por el nombre del environment local!!!
 conda activate gv
 
+#FTYPE=( 'Historical' 'Future' )
+FTYPE=( 'Historical' )
+VARS=( 'tavg' 'tmax' 'tmin' 'prec' 'vapr' 'srad' )
+MONTH=( '01' '02' '03' '04' '05' '06' '07' '08' '09' '10' '11' '12' )
+
 PATH_D='../data/WC/'
 FHEAD='wc2.1_'
 TIF='.tif'
 NC='.nc'
-FNAME=$FHEAD$RES'_'$VAR
-DIR_D=$PATH_D'TIFF/'$FTYPE'/'$FNAME'/'
-FILE_D=$DIR_D$FNAME$TIF
-DIR_R=$PATH_D'NC/'$FTYPE'/'$FNAME'/'
-FILE_R=$DIR_R$FNAME$NC
 
-mkdir -p $DIR_R
-gdal_translate -of NetCDF $FILE_D $FILE_R
+echo 'Begin processing files.'
+
+for f in ${FTYPE[@]}
+do
+    for v in ${VARS[@]}
+    do
+
+        FNAME=$FHEAD$RES'_'$v
+        DIR_D=$PATH_D'TIFF/'$f'/'$FNAME'/'
+        
+        echo ''
+        echo 'Processing '$FNAME'...'
+
+        for m in ${MONTH[@]}
+        do
+
+            FILE_D=$DIR_D$FNAME'_'$m$TIF
+            DIR_R=$PATH_D'NC/'$FTYPE'/'$FNAME'/'
+            FILE_R=$DIR_R$FNAME'_'$m$NC
+
+            mkdir -p $DIR_R
+            gdal_translate -q -of NetCDF $FILE_D $FILE_R
+
+    done
+
+    echo $FNAME' processed.'
+
+    done
+
+    v='elev'
+    FNAME=$FHEAD$RES'_'$v
+    DIR_D=$PATH_D'TIFF/'$f'/'
+    FILE_D=$DIR_D$FNAME$TIF
+    DIR_R=$PATH_D'NC/'$FTYPE'/'$FNAME'/'
+    FILE_R=$DIR_R$FNAME$NC
+
+    echo ''
+    echo 'Processing '$FNAME'...'
+
+    mkdir -p $DIR_R
+    gdal_translate -of NetCDF $FILE_D $FILE_R
+
+    echo ''
+    echo $FNAME' processed.'
+done
+
+echo ''
+echo 'All files processed.'
